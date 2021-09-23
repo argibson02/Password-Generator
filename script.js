bc74ab014cbc297f6626458e191842e92e6aa8dc
Alex Gibson <argibson02@gmail.com>
2021-09-23 09:30:48 -0700

    Added numberic enforcement for func formRunTime.

--- script.js
+++ script.js
@@ -18,13 +18,19 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+//var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+//var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ",", ".", "/", ":", "<", "=", ">", "?", "@", "[", "\"", "\'", "]", "^", "_", "`", "{", "|", "}", "~", ";", "\\"];
+ ///WHY DONT THESE GLOBALS WORK????-------------------------------------
+
 function generatePassword() {
   ////Global Character Arrays////
   var yourPassword = [];
   var globalPasswordArray = [];
   var globalRunTime = 0;
 
-  ////Forming passwordArray Logic////
+  ////Forming globalPasswordArray Logic////
   console.log("--------------------------------------------------------");     //DELETE THIS LINE UPON COMPLETION//
   console.log("formPassArray START");     //DELETE THIS LINE UPON COMPLETION//
 
@@ -58,7 +64,7 @@ function generatePassword() {
       }
     }
     
-    //SPECIAL CHARACTER
+    //SPECIAL CHARACTER//
     var haveSpecial = confirm("Would you like your password to include special characters? Select 'OK' for yes and 'Cancel' for no."); {
       if (haveSpecial === true) {
         var localPasswordArray = localPasswordArray.concat(special);
@@ -83,16 +89,13 @@ function generatePassword() {
 
   function formRunTime() {
   var localRunTime = prompt("Please choose the length of your password. The length must be between 8 and 128 characters."); 
-  
-    /*  ////Need to make error for inputting letter characters//////
-    if (localRunTime typeof number) {
-    alert("You must enter in a number.");
-    return formRunTime();
+    var localRunTime = parseInt(localRunTime)
+      ////Need to make error for inputting letter characters//////
+    if (isNaN(localRunTime)) {  ///HOW TO USE TYPEOF AS ENFORCEMENT???? PROMPT TAKES IN THINGS AS A STRING. ------------------ currently using parseInt and comparing with Nan
+      alert("You must enter in a number.");
+      return formRunTime();
     }
-    else 
-    */
-   
-    if (localRunTime > 128) {
+    else if (localRunTime > 128) {
       alert("The password length must be between 8 and 128 characters. Too high.");
       return formRunTime();
     }
@@ -112,7 +115,7 @@ function generatePassword() {
 
   
   for (let i = 0; i < globalRunTime; i++) {
-    var randoms = globalPasswordArray[Math.floor(Math.random() * globalPasswordArray.length)];
+    var randoms = globalPasswordArray[Math.floor(Math.random() * globalPasswordArray.length)];   ////IS IT BETTER TO PUT FOR LOOPS INSIDE OF FUNCTIONS?--------------------------
     yourPassword = yourPassword.concat(randoms);
   }
   console.log(yourPassword);  //DELETE THIS LINE UPON COMPLETION//
